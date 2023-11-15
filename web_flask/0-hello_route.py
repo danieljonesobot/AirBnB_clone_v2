70f05d545df932b50ff7d2348f1980daa515ac8a
danieljonesobot <daniel.jonesobot@gmail.com>
2023-11-15 17:27:34 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" This script sets the home url to hello HBNB!$
-Application is listening on 0.0.0.0, port 5000
+""" Starts a web flask application
 """
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+@app.route('/airbnb-onepage/',strict_slashes=False)
 def hello_HBNB():
-	return "Hello HBNB!"
+    '''Displays Hello HBNB'''
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
